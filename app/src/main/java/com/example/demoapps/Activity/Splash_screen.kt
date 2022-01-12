df41f07abd85037e5a672710139b7194501dcf5c
mrcoderrrrr <psatardekar0192000@gmail.com>
2022-01-12 13:50:54 +0530

    commit demoapp

---
+++ app/src/main/java/com/example/demoapps/Activity/Splash_screen.kt
@@ -0,0 +1,36 @@
+package com.example.demoapps
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
+import com.example.demoapps.R
+import com.example.demoapps.databinding.ActivitySplashScreenBinding
+
+class splash_screen : AppCompatActivity() {
+    lateinit var databinding : ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        databinding=DataBindingUtil.setContentView(this,R.layout.activity_splash_screen)
+        //animate logo
+        databinding.ivLogo.animate().apply {
+            duration = 3000
+            rotationYBy(360f)
+        }.start()
+
+        //animate logo txt
+        val tv_animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        databinding.tvLogoTxt.startAnimation(tv_animation)
+
+
+        //open mainactivity using handler
+        android.os.Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
