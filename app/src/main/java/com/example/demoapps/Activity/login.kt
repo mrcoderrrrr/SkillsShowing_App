47b58d99fd8b295b225f26ed242974feaa950810
mrcoderrrrr <psatardekar0192000@gmail.com>
2022-01-13 12:11:24 +0530

    commit 13jan 12:11

--- app/src/main/java/com/example/demoapps/Activity/login.kt
+++ app/src/main/java/com/example/demoapps/Activity/login.kt
@@ -2,6 +2,7 @@ package com.example.demoapps.Activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -26,12 +27,34 @@ class login_activity : AppCompatActivity() {
         val email = databinding.teEmail.text.toString()
         val password = databinding.tePassword.text.toString()
         databinding.butLogin.setOnClickListener {
+            if (email.toString().isEmpty()){
+                Log.d("Emailtxt",email)
+                Log.d("Emaildata",databinding.teEmail.text.toString())
+             databinding.teEmail.setError("Username is mandatory")
+            }
+            else if (password.isEmpty()) {
+                databinding.tePassword.setError("Password is manadatory")
+            }
+            else {
+                Log.d("Emailtxt1",email)
+                Log.d("Emaildata1",databinding.teEmail.text.toString())
+                Log.d("Passtxt1",password)
+                Log.d("Passdata1",databinding.tePassword.text.toString())
+                if (email.equals("admin") && password.equals("admin")) {
+                    Log.d("Emailtxt",email)
+                    Log.d("Emaildata",databinding.teEmail.text.toString())
+                    Log.d("Passtxt",password)
+                    Log.d("Passdata",databinding.tePassword.text.toString())
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                     Toast.makeText(this, "Log in Sucessfull", Toast.LENGTH_LONG).show()
-                    onBackPressed()
+                }
+                else{
+                    Toast.makeText(this, "Log in Faild", Toast.LENGTH_LONG).show()
+                }
             }
         }
+    }
 
     private fun SignUp() {
         databinding.tvSignupTxt.setOnClickListener {
