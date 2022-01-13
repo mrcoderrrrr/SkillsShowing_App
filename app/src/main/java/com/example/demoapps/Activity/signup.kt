dde41f9a1f3b481976a79ddff9c9402e31ab7aea
mrcoderrrrr <psatardekar0192000@gmail.com>
2022-01-13 11:41:07 +0530

    commit 13jan 11:40

--- app/src/main/java/com/example/demoapps/Activity/signup.kt
+++ app/src/main/java/com/example/demoapps/Activity/signup.kt
@@ -1,42 +1,62 @@
 package com.example.demoapps.Activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.demoapps.R
-import com.example.demoapps.databinding.ActivityLoginBinding
 import com.example.demoapps.databinding.ActivitySignupBinding
 
 class signup : AppCompatActivity() {
-    lateinit var databinding:ActivitySignupBinding
+    lateinit var databinding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        databinding=DataBindingUtil.setContentView(this,R.layout.activity_signup)
+        databinding = DataBindingUtil.setContentView(this, R.layout.activity_signup)
 
-        //Enter Details
-        SignUpDetails()
+        //error message
+        errormsg()
     }
 
-    private fun SignUpDetails() {
+    //Error Message
+    private fun errormsg() {
         databinding.butSignup.setOnClickListener {
-            val name=databinding.teName.text.toString()
-            val email=databinding.teEmail.text.toString()
-            val dob=databinding.teBirthdate.text.toString()
-            val password=databinding.tePassword.text.toString()
-            val confirm_password=databinding.teConfirmPassword.text.toString()
-            val male=databinding.rbutMale
-            val female=databinding.rbutFemale
-            if(name.isEmpty() || email.isEmpty() || dob.isEmpty() || password.isEmpty() || confirm_password.isEmpty()){
-               databinding.teEmail.setError("Email is Mandatory")
-                databinding.tePassword.setError("Password is Mandatory")
-                databinding.teConfirmPassword.setError("Passworod is Mandatory")
-                Toast.makeText(this,"Fill the Detalis",Toast.LENGTH_LONG).show()
-            }
-            else{
-                Toast.makeText(this,"Detalis Are Submitted",Toast.LENGTH_LONG).show()
-            }
+            val name = databinding.teName.text.toString()
+            val email = databinding.teEmail.text.toString()
+            val dob = databinding.teBirthdate.text.toString()
+            val password = databinding.tePassword.text.toString()
+            val confirm_password = databinding.teConfirmPassword.text.toString()
+            val male = databinding.rbutMale
+            val female = databinding.rbutFemale
 
+            if (name.isEmpty()) {
+                databinding.teName.setError("Name is Mandatory")
+            } else if (email.isEmpty()) {
+                databinding.teEmail.setError("Email is Mandatory")
+            } else if (dob.isEmpty()) {
+                databinding.teBirthdate.setError("Birthdate is Mandatory")
+            } else if (password.isEmpty()) {
+                databinding.tePassword.setError("Password is Mandatory")
+            } else if (confirm_password.isEmpty()) {
+                databinding.teConfirmPassword.setError("Passworod is Mandatory")
+            } else {
+                Toast.makeText(this, "Detalis Are Submitted", Toast.LENGTH_LONG).show()
+            }
+            //Gender value
+            gender(male,female)
+        }
+    }
+
+    //Gender
+    private fun gender(male: RadioButton, female: RadioButton) {
+        if (male.isChecked) {
+            var Male = "Male"
+        } else if (female.isChecked) {
+            var Female = "Female"
+        } else {
+            if (!male.isChecked && !female.isChecked) {
+                databinding.tvGenderTxt.setError("Gender selection are mandatory")
+            }
         }
     }
 }
