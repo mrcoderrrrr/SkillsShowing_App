3390164aee97bd6a438823d2b6787f779e321d9d
mrcoderrrrr <psatardekar0192000@gmail.com>
2022-01-25 16:14:06 +0530

    comiit 25jan 4:15

--- app/src/main/java/com/example/demoapps/activity/AddUser.kt
+++ app/src/main/java/com/example/demoapps/activity/AddUser.kt
@@ -1,13 +1,53 @@
 package com.example.demoapps.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.demoapps.R
+import com.example.demoapps.databinding.ActivityAddUserBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddUser : AppCompatActivity() {
+    private lateinit var dataBinding: ActivityAddUserBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_user)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_add_user)
 
+
+        setClick()
     }
+
+    private fun setClick() {
+        DateOfBirth()
+    }
+
+    private fun DateOfBirth() {
+
+        val myCalendar = Calendar.getInstance()
+        val datePicker = DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, day)
+            updateDate(myCalendar)
+        }
+        dataBinding.teBirthdate.setOnClickListener {
+            DatePickerDialog(
+                this,
+                datePicker,
+                myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+    }
+
+    private fun updateDate(myCalendar: Calendar) {
+        val myformat = "dd-MM-yyyy"
+        val sdf = SimpleDateFormat(myformat, Locale.UK)
+        dataBinding.teBirthdate.setText(sdf.format(myCalendar.time))
+    }
+
+
 }
--- app/src/main/java/com/example/demoapps/activity/SignUp.kt
+++ app/src/main/java/com/example/demoapps/activity/SignUp.kt
@@ -1,5 +1,6 @@
 package com.example.demoapps.activity
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -10,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.demoapps.R
 import com.example.demoapps.databinding.ActivitySignupBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SignUp : AppCompatActivity() {
     private lateinit var dataBinding: ActivitySignupBinding
@@ -29,7 +32,7 @@ class SignUp : AppCompatActivity() {
         Log.d("ShareValue", dataBinding.teName.text.toString() + dataBinding.teEmail.text.toString() + dataBinding.tePassword.toString() + dataBinding.teConfirmPassword.text.toString())
         editor.apply()
         //Gender Data
-        setSignUpData()
+
         Log.d("Data", sharedPreference.getString("Email","").toString())
 
         setLoginScreen()
@@ -43,33 +46,53 @@ class SignUp : AppCompatActivity() {
     }
 
     private fun setSignUpData() {
-        dataBinding.teBirthdate.setOnClickListener{
         DatePicker()
-        }
     }
 
     private fun DatePicker() {
+        val myCalendar = Calendar.getInstance()
+        val datePicker = DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, day)
+            updateDate(myCalendar)
+        }
+        dataBinding.tlBirthdate.setOnClickListener {
+            DatePickerDialog(
+                this,
+                datePicker,
+                myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+    }
 
+    private fun updateDate(myCalendar: Calendar) {
+        val myformat = "dd-MM-yyyy"
+        val sdf = SimpleDateFormat(myformat, Locale.UK)
+        dataBinding.teBirthdate.setText(sdf.format(myCalendar.time))
     }
 
     private fun setClick() {
         dataBinding.btnSignup.setOnClickListener {
             if (validate()) {
                 setSharePreference()
+                setSignUpData()
             }
         }
     }
 
     private fun validate(): Boolean {
-            if (dataBinding.teName.text.toString().isEmpty()) {
+            if (dataBinding.teName.text.toString() == null ) {
                 dataBinding.teName.setError("Name is Mandatory")
-            } else if (dataBinding.teEmail.text.toString().isEmpty()) {
+            } else if (dataBinding.teEmail.text.toString() == null) {
                 dataBinding.teEmail.setError("Email is Mandatory")
-            } else if (dataBinding.teBirthdate.text.toString().isEmpty()) {
+            } else if (dataBinding.teBirthdate.text.toString() == null) {
                 dataBinding.teBirthdate.setError("Birthdate is Mandatory")
-            } else if (dataBinding.tePassword.text.toString().isEmpty()) {
+            } else if (dataBinding.tePassword.text.toString() == null) {
                 dataBinding.tePassword.setError("Password is Mandatory")
-            } else if (dataBinding.teConfirmPassword.text.toString().isEmpty()) {
+            } else if (dataBinding.teConfirmPassword.text.toString() == null) {
                 dataBinding.teConfirmPassword.setError("Password is Mandatory")
             } else if (!dataBinding.rbutMale.isChecked && !dataBinding.rbutFemale.isChecked) {
                 dataBinding.tvGenderTxt.setError("Gender selection are mandatory")
